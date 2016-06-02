f9deb7cee0f86e40756b3bdba0799de260d02425
libertas <horizonvei@gmail.com>
2016-06-02 17:19:23 +0800

    Update menubar Use JScrollPane for the editor

--- src/Jedit.java
+++ src/Jedit.java
@@ -1,36 +1,55 @@
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
-
-import javax.swing.JButton;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 public class Jedit {
 	private static JTextArea editor;
-	private static Scanner s = null;;
 	
 	public static void main(String[] arg) {
-		JFrame app = new JFrame("Editor");
+		JFrame app = new JFrame("JEdit");
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		app.setSize(500, 670);
 		
 		Container c = app.getContentPane();
-		c.setLayout(new FlowLayout());
+		c.setLayout(new BoxLayout(c, 1));
+
+		JMenuBar menubar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		menubar.add(fileMenu);
+		
+		JMenuItem newItem = new JMenuItem("New");
+		fileMenu.add(newItem);
+		
+		JMenuItem openItem = new JMenuItem("Open");
+		fileMenu.add(openItem);
+		
+		JMenuItem saveItem = new JMenuItem("Save");
+		fileMenu.add(saveItem);
+		
+		JMenuItem exitItem = new JMenuItem("Exit");
+		fileMenu.add(exitItem);
+		
+		
+		JMenu helpMenu = new JMenu("Help");
+		menubar.add(helpMenu);
+		
+		JMenuItem aboutItem = new JMenuItem("About");
+		helpMenu.add(aboutItem);
+		
+		c.add(menubar);
 		
 		editor = new JTextArea("", 35, 40);
 		editor.setBorder(new LineBorder(Color.black));
-		c.add(editor);
+		JScrollPane editSP = new JScrollPane(editor);
+		c.add(editSP);
 		
 		app.setVisible(true);
 	}
